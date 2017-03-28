1076bd79248d118bc73787d23ac0d9a7a95bfc01
mazerty <mazerty@gmail.com>
2017-03-28 18:36:55 +0200

    added postgresql layers

---
+++ build/resources/postgresql/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+data=/volume/data
+bin=/usr/lib/postgresql/9.5/bin
+
+export IDEM_DIR=/volume/.idem
+idem.sh "mkdir $data && chown postgres:postgres $data"
+idem.sh "gosu postgres $bin/initdb --locale=C.UTF-8 -D $data"
+
+exec gosu postgres $bin/postgres -D $data
