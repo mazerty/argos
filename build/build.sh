428b6d6840b4b2485bbf3e42b50ce9247b3bd8c7
mazerty <mazerty@gmail.com>
2017-04-01 17:01:42 +0200

    added awscli build

--- build/build.sh
+++ build/build.sh
@@ -11,4 +11,6 @@ build mazerty/wildfly-postgresql core jdk wildfly wildfly-postgresql
 build mazerty/deploy-torii       core jdk maven-git deploy-torii
 build mazerty/nginx              core nginx
 
+build mazerty/awscli             core dev awscli
+
 rm Dockerfile
