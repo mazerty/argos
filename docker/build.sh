fd82298702135ad8491a6913af4a3fa62c89fcbc
mazerty <mazerty@gmail.com>
2017-05-07 09:34:41 +0200

    comments

--- docker/build.sh
+++ docker/build.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
-set -e
+set -ex
 
 build() {
+  # creates the Dockerfile from the various layers
   cat ${@:2} > Dockerfile
+
+  # builds the image
   docker build -t $1 .
 
+  # pushes the image into the registry
   docker push $1
+
+  # cleans the dockerfile
+  rm Dockerfile
 }
 
-docker login
+# loads docker cache from s3
 aws --region eu-central-1 s3 cp s3://mazerty-argos/cache.tar.gz - | gunzip | docker load
 
+# logs into docker registry
+docker login
+
+# builds production images
 build mazerty/postgresql         core postgresql
 build mazerty/wildfly-postgresql core jdk wildfly wildfly-postgresql
 build mazerty/maven-torii        core jdk maven maven-torii
 build mazerty/nginx              core nginx
 
+# builds development images
 build mazerty/intellij-torii     core jdk wildfly wildfly-postgresql maven intellij intellij-torii
 
+# saves docker cache into s3
 docker save $(docker image ls -qa) | gzip | aws --region eu-central-1 s3 cp - s3://mazerty-argos/cache.tar.gz
-rm Dockerfile
