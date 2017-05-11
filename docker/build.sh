043fb1c24f939dd3126ab9b4b164caeb9695e717
mazerty <mazerty@gmail.com>
2017-05-11 11:24:26 +0200

    optimizes things a bit

--- docker/build.sh
+++ docker/build.sh
@@ -11,15 +11,16 @@ build() {
   # pushes the image into the registry
   docker push $1
 
-  # cleans the dockerfile
+  # cleans some stuff
   rm Dockerfile
+  docker image prune -f
 }
-
-# loads docker cache from s3
-aws --region eu-central-1 s3 cp s3://mazerty-argos/cache.tar.gz - | gunzip | docker load
 
 # logs into docker registry
 docker login
+
+# loads docker cache from s3
+aws --region eu-central-1 s3 cp s3://mazerty-argos/cache.tar.gz - | gunzip | docker load
 
 # builds production images
 build mazerty/postgresql         core postgresql
@@ -31,6 +32,5 @@ build mazerty/nginx              core nginx
 build mazerty/intellij-torii     core jdk maven intellij wildfly wildfly-postgresql intellij-torii
 
 # saves docker cache into s3
-docker image prune
 docker save $(docker image ls -qa) | gzip > /tmp/cache.tar.gz
 aws --region eu-central-1 s3 cp /tmp/cache.tar.gz s3://mazerty-argos/cache.tar.gz
