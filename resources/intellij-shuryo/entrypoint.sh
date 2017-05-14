15638573750ca7056009ae72eb5ec10b91041437
mazerty <mazerty@gmail.com>
2017-05-14 17:47:33 +0200

    now shuryo has its own repo

--- resources/intellij-shuryo/entrypoint.sh
+++ resources/intellij-shuryo/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -e
 
-idem.sh "mvn-build.sh java-core  install" /workspace/.idem
-idem.sh "mvn-build.sh shika-kami install" /workspace/.idem
-idem.sh "mvn-build.sh shika-ishi install" /workspace/.idem
+idem.sh "mvn-build.sh java-core    install" /workspace/.idem
+idem.sh "mvn-build.sh shika-kami   install" /workspace/.idem
+idem.sh "mvn-build.sh shika-ishi   install" /workspace/.idem
+idem.sh "mvn-build.sh shika-shuryo wildfly:deploy" /workspace/.idem
 
 exec /opt/intellij/bin/idea.sh
--- resources/intellij-shuryo/redeploy.sh
+++ resources/intellij-shuryo/redeploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
 
-mvn-build.sh java-core  clean install
-mvn-build.sh shika-kami clean install
-mvn-build.sh shika-ishi clean install
+mvn-build.sh java-core    clean install
+mvn-build.sh shika-kami   clean install
+mvn-build.sh shika-ishi   clean install
+mvn-build.sh shika-shuryo clean wildfly:deploy
