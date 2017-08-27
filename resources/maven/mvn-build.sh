dd7085939082ee119b24d5747cb84e79a63cb415
mazerty <mazerty@gmail.com>
2017-08-27 18:30:53 +0200

    maven prepared scripts are no longer useful with the new idem.py and the new versioning via nexus

--- resources/maven/mvn-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-[ -d $1 ] || git clone git@github.com:mazerty/$1.git
-cd $1
-[ -n $BRANCH ] && git checkout $BRANCH
-mvn ${@:2}
--- resources/maven/mvn-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-[ -d $1 ] || git clone https://github.com/mazerty/$1.git
-cd $1
-[ -n $BRANCH ] && git checkout $BRANCH
-mvn ${@:2}
-cd ..
-rm -rf $1
