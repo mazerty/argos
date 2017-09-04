9581101601845cda3bf547b2cbe3a22b3f6a4351
mazerty <mazerty@gmail.com>
2017-09-04 19:19:14 +0200

    fix syntax error

--- resources/postgresql/entrypoint.sh
+++ resources/postgresql/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
-$data=/volume/data
-$bin=/usr/lib/postgresql/9.5/bin
+data=/volume/data
+bin=/usr/lib/postgresql/9.5/bin
 
 [ ! -d $data ] && mkdir $data && chown postgres:postgres $data && gosu postgres $bin/initdb --locale=C.UTF-8 -D $data
 
